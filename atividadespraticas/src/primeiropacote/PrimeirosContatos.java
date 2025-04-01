9bc9d0d964936615d0427216244b9fdf513f1f21
Adler Almeida <adler-almeida@hotmail.com>
2025-04-01 16:43:07 -0300

    Initial Commit

--- atividadespraticas/src/primeiropacote/PrimeirosContatos.java
+++ atividadespraticas/src/primeiropacote/PrimeirosContatos.java
@@ -1,7 +1,17 @@
 package primeiropacote;
 
+import java.util.Date;
+
 public class PrimeirosContatos {
     public static void main(String[] args) {
+
         System.out.println("Olá, World");
+
+        System.out.println();
+        Date relogio = new Date ();        // classe Date, new é criar um novo objeto, que é elogio.
+        System.out.println("A hora do sistema é");
+
+        System.out.println();
+        System.out.println(relogio.toString());
     }
 }
